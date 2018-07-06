9e783af5426a082bb8544b615f65a01cee504ab8
ppoyang <yourmyeden@gamil.com>
2018-07-06 14:25:39 +0900

    spring class

---
+++ s02_1/src/main/java/s02/MyCal.java
@@ -0,0 +1,52 @@
+package s02;
+
+public class MyCal {
+	
+	Calculator cal;
+	private int firstNum;
+	private int secondNum;
+	
+	public MyCal() {
+		
+	}
+	
+	public Calculator getCal() {
+		return cal;
+	}
+
+	public void setCal(Calculator cal) {
+		this.cal = cal;
+	}
+
+	public void add() {
+		cal.addition(firstNum,secondNum);
+	}
+	
+	public void sub() {
+		cal.subtraction(firstNum,secondNum);
+	}
+	
+	public void mul() {
+		cal.multiplication(firstNum,secondNum);
+	}
+	
+	public void div() {
+		cal.division(firstNum,secondNum);
+	}
+
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+}
---
+++ spring_0706/src/main/java/spring_0706/Hello.java
@@ -0,0 +1,10 @@
+package spring_0706;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("dfdf");
+
+	}
+
+}
